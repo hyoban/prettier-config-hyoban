04bd556d6e09aa40c9bbc55c5c477d7c893a8fee
Stephen Zhou <hi@hyoban.cc>
2024-04-13 17:26:22 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,56 @@
+const GLOB_EXCLUDE = [
+	"**/node_modules",
+	"**/dist",
+	"**/package-lock.json",
+	"**/yarn.lock",
+	"**/pnpm-lock.yaml",
+	"**/bun.lockb",
+
+	"**/output",
+	"**/coverage",
+	"**/temp",
+	"**/.temp",
+	"**/tmp",
+	"**/.tmp",
+	"**/.history",
+	"**/.vitepress/cache",
+	"**/.nuxt",
+	"**/.next",
+	"**/.vercel",
+	"**/.changeset",
+	"**/.idea",
+	"**/.cache",
+	"**/.output",
+	"**/.vite-inspect",
+	"**/.yarn",
+
+	"**/CHANGELOG*.md",
+	"**/*.min.*",
+	"**/LICENSE*",
+	"**/__snapshots__",
+	"**/auto-import?(s).d.ts",
+	"**/components.d.ts",
+];
+
+/** @type {import('prettier').Options} */
+module.exports = {
+	experimentalTernaries: true,
+	useTabs: true,
+	quoteProps: "consistent",
+
+	// Defaults
+	semi: true,
+	singleQuote: false,
+	jsxSingleQuote: false,
+	trailingComma: "all",
+	arrowParens: "always",
+	endOfLine: "lf",
+	overrides: [
+		{
+			files: GLOB_EXCLUDE,
+			options: {
+				requirePragma: true,
+			},
+		},
+	],
+};
