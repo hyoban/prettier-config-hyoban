401c082cbc747c05935f636534983456a89dd915
Stephen Zhou <hi@hyoban.cc>
2024-04-20 20:15:48 +0800

    chore: format

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
 const GLOB_EXCLUDE = [
-	"**/node_modules",
-	"**/dist",
-	"**/package-lock.json",
-	"**/yarn.lock",
-	"**/pnpm-lock.yaml",
-	"**/bun.lockb",
+  "**/node_modules",
+  "**/dist",
+  "**/package-lock.json",
+  "**/yarn.lock",
+  "**/pnpm-lock.yaml",
+  "**/bun.lockb",
 
-	"**/output",
-	"**/coverage",
-	"**/temp",
-	"**/.temp",
-	"**/tmp",
-	"**/.tmp",
-	"**/.history",
-	"**/.vitepress/cache",
-	"**/.nuxt",
-	"**/.next",
-	"**/.vercel",
-	"**/.changeset",
-	"**/.idea",
-	"**/.cache",
-	"**/.output",
-	"**/.vite-inspect",
-	"**/.yarn",
+  "**/output",
+  "**/coverage",
+  "**/temp",
+  "**/.temp",
+  "**/tmp",
+  "**/.tmp",
+  "**/.history",
+  "**/.vitepress/cache",
+  "**/.nuxt",
+  "**/.next",
+  "**/.vercel",
+  "**/.changeset",
+  "**/.idea",
+  "**/.cache",
+  "**/.output",
+  "**/.vite-inspect",
+  "**/.yarn",
 
-	"**/CHANGELOG*.md",
-	"**/*.min.*",
-	"**/LICENSE*",
-	"**/__snapshots__",
-	"**/auto-import?(s).d.ts",
-	"**/components.d.ts",
-	"**/eslint-typegen.d.ts",
+  "**/CHANGELOG*.md",
+  "**/*.min.*",
+  "**/LICENSE*",
+  "**/__snapshots__",
+  "**/auto-import?(s).d.ts",
+  "**/components.d.ts",
+  "**/eslint-typegen.d.ts",
 ];
 
 /** @type {import('prettier').Options} */
 module.exports = {
-	overrides: [
-		{
-			files: GLOB_EXCLUDE,
-			options: {
-				requirePragma: true,
-			},
-		},
-		{
-			files: ["*.json", "*.json5", "*.jsonc", ".eslintrc"],
-			options: {
-				trailingComma: "none",
-			},
-		},
-	],
+  overrides: [
+    {
+      files: GLOB_EXCLUDE,
+      options: {
+        requirePragma: true,
+      },
+    },
+    {
+      files: ["*.json", "*.json5", "*.jsonc", ".eslintrc"],
+      options: {
+        trailingComma: "none",
+      },
+    },
+  ],
 };
